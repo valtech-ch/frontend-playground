0e78e002fe841ce3f06c939cb87ed63bada7610c
Samuel Gomez <samuel.gomez@valtech.com>
2020-07-31 20:45:25 +0200

    Footer layout closer to UX spec

--- packages/blog-with-markdown-files/components/organisms/Footer.tsx
+++ packages/blog-with-markdown-files/components/organisms/Footer.tsx
@@ -3,18 +3,24 @@ import styled from '@emotion/styled'
 import tw from '@tailwindcssinjs/macro'
 
 const Footer = styled.footer(tw`bg-black text-white flex justify-center w-full`)
-const FooterContent = styled.div(tw`w-64`)
+const FooterContent = styled.div(tw`ml-8 border-4 flex`)
+const FooterAvatar = styled.div(tw`flex-initial`)
+const FooterText = styled.div(tw`flex-initial`)
 
 export default function BlankLayout(): React.ReactElement {
   return (
     <Footer>
       <FooterContent>
-        <Img alt="" src="https://ca.slack-edge.com/T04UL2109-UUR631KMK-bfb35a90cec0-512"></Img>
-        <strong>Jason Wilson</strong>
-        <p>
-          Frontend Developer with Valtech, Web Generalist, and former Maritime Officer. I love to solve problems and
-          pick up new tools along the way.
-        </p>
+        <FooterAvatar>
+          <Img alt="" src="https://ca.slack-edge.com/T04UL2109-UUR631KMK-bfb35a90cec0-512"></Img>
+        </FooterAvatar>
+        <FooterText>
+          <strong>Jason Wilson</strong>
+          <p>
+            Frontend Developer with Valtech, Web Generalist, and former Maritime Officer. I love to solve problems and
+            pick up new tools along the way.
+          </p>
+        </FooterText>
       </FooterContent>
     </Footer>
   )
