ead4d80b48593d39a2cff026832c109331754551
Samuel Gomez <samuel.gomez@valtech.com>
2020-07-31 16:33:55 +0200

    More work on footer towards UX spec

--- packages/blog-with-markdown-files/components/organisms/Footer.tsx
+++ packages/blog-with-markdown-files/components/organisms/Footer.tsx
@@ -1,15 +1,20 @@
-import Logo from 'components/molecules/Logo'
+import Img from 'components/atoms/Img'
 import styled from '@emotion/styled'
 import tw from '@tailwindcssinjs/macro'
 
-const Footer = styled.footer(tw`bg-black flex justify-center w-full`)
+const Footer = styled.footer(tw`bg-black text-white flex justify-center w-full`)
 const FooterContent = styled.div(tw`w-64`)
 
 export default function BlankLayout(): React.ReactElement {
   return (
     <Footer>
       <FooterContent>
-        <Logo />
+        <Img alt="" src="https://ca.slack-edge.com/T04UL2109-UUR631KMK-bfb35a90cec0-512"></Img>
+        <strong>Jason Wilson</strong>
+        <p>
+          Frontend Developer with Valtech, Web Generalist, and former Maritime Officer. I love to solve problems and
+          pick up new tools along the way.
+        </p>
       </FooterContent>
     </Footer>
   )
