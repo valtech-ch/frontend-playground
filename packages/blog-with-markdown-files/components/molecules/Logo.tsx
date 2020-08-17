b8f4d6a1ac10e8e45561dfbdb32d3d5080e6368b
Jason Wilson <jason.wilson@valtech.com>
2020-08-17 14:15:27 +0200

    chore(blog): Update Logo image
    - adds base and 2x side valtech_fe logo
    - updates Logo component with new image

--- packages/blog-with-markdown-files/components/molecules/Logo.tsx
+++ packages/blog-with-markdown-files/components/molecules/Logo.tsx
@@ -4,7 +4,7 @@ import Link from 'components/atoms/Link'
 export default function Logo(): React.ReactElement {
   return (
     <Link href="/">
-      <Img src="/logo.png" alt="Valtech Logo" />
+      <Img src="/logo@2x.png" alt="Valtech Logo" />
     </Link>
   )
 }
