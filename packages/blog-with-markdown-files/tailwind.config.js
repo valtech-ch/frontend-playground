88fa49cbacd7a1e993fd6016429c1952fdc09512
Jason Wilson <jason.wilson@valtech.com>
2020-08-17 14:45:41 +0200

    feat(Issue #12): Apply color scheme to whole theme

--- packages/blog-with-markdown-files/tailwind.config.js
+++ packages/blog-with-markdown-files/tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   theme: {
-    borderColor: (theme) => ({
-      ...theme('colors'),
-      'valtech-blue': 'rgb(0, 189, 250)',
-      'valtech-cyan': 'rgb(0, 255, 250)',
-      'valtech-rose': 'rgb(255, 27, 120)',
-      'valtech-turquoise': 'rgb(13, 253, 180)',
-      'valtech-yellow': 'rgb(255, 240, 17)',
-    }),
+    extend: {
+      colors: {
+        'valtech-blue': 'rgb(0, 189, 250)',
+        'valtech-cyan': 'rgb(0, 255, 250)',
+        'valtech-rose': 'rgb(255, 27, 120)',
+        'valtech-turquoise': 'rgb(13, 253, 180)',
+        'valtech-yellow': 'rgb(255, 240, 17)',
+      },
+    },
   },
   variants: {},
   plugins: [require('@tailwindcss/ui')],
