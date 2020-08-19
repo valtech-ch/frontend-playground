7d02853ac52532c1c530386258ea371ae8e7276a
Jason Wilson <jason.wilson@valtech.com>
2020-08-19 18:04:43 +0200

    feat(Blog): Add Button styles to Link Component

--- packages/blog-with-markdown-files/components/atoms/Link.tsx
+++ packages/blog-with-markdown-files/components/atoms/Link.tsx
@@ -1,15 +1,21 @@
 import Link_ from 'next/Link'
+import styled from '@emotion/styled'
+import tw from '@tailwindcssinjs/macro'
 
 type LinkProps = {
   children: React.ReactChild
   href: string
   as?: string
+  button?: boolean
 }
 
-export default function Link({ children, href, as }: LinkProps): React.ReactElement {
+export default function Link({ children, href, as, button = false }: LinkProps): React.ReactElement {
+  const buttonClass = button ? 'link--button-link' : undefined
+  const Anchor_ = styled.a(tw`cursor-pointer`)
+
   return (
     <Link_ href={href} as={as}>
-      <a>{children}</a>
+      <Anchor_ className={buttonClass}>{children}</Anchor_>
     </Link_>
   )
 }
