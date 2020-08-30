db2e0d4d302d569b0c1bc79c596fdd747fa8f424
Jason Wilson <jason.wilson@valtech.com>
2020-08-30 22:19:59 +0200

    feat(Blog): Clean Up Post List Styles

--- packages/blog-with-markdown-files/components/organisms/PostList/PostList.tsx
+++ packages/blog-with-markdown-files/components/organisms/PostList/PostList.tsx
@@ -7,15 +7,17 @@ import PostListItem from 'components/organisms/PostListItem/PostListItem'
 const PostList_ = styled.ul(tw`flex w-full flex-wrap justify-between items-center`)
 
 type PostListProps = {
-  blogPosts: BlogPost[]
+	blogPosts: BlogPost[]
 }
 
 export default function PostList({ blogPosts }: PostListProps): React.ReactElement {
-  return (
-    <PostList_>
-      {blogPosts.map((post) => {
-        return <PostListItem blogPost={post} key={post.id}></PostListItem>
-      })}
-    </PostList_>
-  )
+	return (
+		<PostList_>
+			{blogPosts
+				.filter((post) => post.published)
+				.map((post, index) => {
+					return <PostListItem blogPost={post} index={index} key={post.id}></PostListItem>
+				})}
+		</PostList_>
+	)
 }
