c7bc495d47fc693e018a31bcb74cab252f60042a
Lorenzo García Moreno <lorenzo.garciamoreno@valtech.com>
2020-09-15 23:25:52 +0200

    feat(blog-posts): - get slug and date from the filename - use local featured images

--- packages/blog-with-markdown-files/constants/blog.ts
+++ packages/blog-with-markdown-files/constants/blog.ts
@@ -1,1 +1,2 @@
 export const CONTENTS_BLOG_POSTS_PATH = 'contents/blog-posts'
+export const IMAGES_BLOG_POSTS_PATH = 'images/blog-posts'
--- packages/blog-with-markdown-files/pages/index.tsx
+++ packages/blog-with-markdown-files/pages/index.tsx
@@ -1,5 +1,5 @@
 import { BlogPost } from 'types/blog'
-import { CONTENTS_BLOG_POSTS_PATH } from 'constants/blog'
+import { CONTENTS_BLOG_POSTS_PATH, IMAGES_BLOG_POSTS_PATH } from 'constants/blog'
 import { GetStaticProps } from 'next'
 import PostList from 'components/organisms/PostList/PostList'
 import React from 'react'
@@ -20,25 +20,32 @@ export const getStaticProps: GetStaticProps = async () => {
 	const files = fs.readdirSync(`${process.cwd()}/${CONTENTS_BLOG_POSTS_PATH}`, 'utf-8')
 
 	const blogPosts: BlogPost[] = files
-		.filter((fn) => fn.endsWith('.md'))
-		.map((fn) => {
-			const path = `${process.cwd()}/${CONTENTS_BLOG_POSTS_PATH}/${fn}`
+		// just MD files
+		.filter((fileName) => fileName.endsWith('.md'))
+		// files starting with the date
+		.filter((fileName) => fileName.match(/\d{4}.\d{2}.\d{2}/) !== null)
+		.map((fileName) => {
+			const path = `${process.cwd()}/${CONTENTS_BLOG_POSTS_PATH}/${fileName}`
+			const [date] = fileName.match(/\d{4}.\d{2}.\d{2}/)
 			const rawContent = fs.readFileSync(path, {
 				encoding: 'utf-8',
 			})
 			const { data } = matter(rawContent)
 
+			const featuredImagePath = `${IMAGES_BLOG_POSTS_PATH}/${fileName.replace('.md', '.jpg')}`
+
 			return {
 				id: uuid(),
-				slug: data.slug,
+				slug: fileName.replace('.md', ''),
 				title: data.title,
 				author: data.author,
 				published: data.published,
-				publishDate: data.publishDate,
-				featuredImage: data.featuredImage || null,
+				publishDate: date,
+				featuredImage: fs.existsSync(`${process.cwd()}/public/${featuredImagePath}`) ? `./${featuredImagePath}` : null,
 				tags: data.tags || ['Blog'],
 			} as BlogPost
 		})
+		.filter((blogPost) => blogPost.published)
 		.sort((firstPost, nextPost) => {
 			return Date.parse(nextPost.publishDate) - Date.parse(firstPost.publishDate)
 		})
