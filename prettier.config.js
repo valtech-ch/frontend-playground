208c9ea1eac0239f04c11437a2f50cc37c724294
Lorenzo García Moreno <lorenzo.garciamoreno@valtech.com>
2020-09-18 21:57:23 +0200

    config: setup dev tools for a monorepo

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// watch out! there is a ".editorconfig" file too
+
+module.exports = {
+  endOfLine: 'auto',
+  trailingComma: 'all',
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  arrowParens: 'always',
+}
